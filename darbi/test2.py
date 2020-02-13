f0f790c36d40df963611923d0e94ef786cdfe0f0
user <arius005@inbox.lv>
2020-02-13 14:03:29 +0200

    20200213_15_03

---
+++ darbi/test2.py
@@ -0,0 +1,10 @@
+veseli_sk = 21//4
+print(veseli_sk)
+atlikums = 21%4
+print(atlikums)
+yeet = 'yeet '
+stop = 'stop '
+print(yeet+stop)
+print(yeet*veseli_sk)
+age = input('cik tev gadu?\n')
+print('%s gadi'%(age))
